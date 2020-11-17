e4dc2510619dd2ee4da133d86ce909a1984b6162
Pinatakid <ondrejvlasek1@gmail.com>
2020-11-17 12:36:35 +0100

    test

--- app/src/main/java/com/example/omp_hra/MainActivity.java
+++ app/src/main/java/com/example/omp_hra/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.omp_hra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
